9a1997b2a15acf7a59406dcf7d176790ef6d0d22
Muneeb <muneeb.zubair.hash@gmail.com>
2024-05-24 18:18:10 +0500

    Home Componet, Housing Card Componet NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent, HousingLocationComponent],
   template: `
-    <h1>Hello</h1>
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'homes';
 }
