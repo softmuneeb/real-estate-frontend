81af614e89fbc2f59bc9c126e9639375f69a46c1
Muneeb <muneeb.zubair.hash@gmail.com>
2024-05-25 16:40:29 +0500

    level up: angular forms done

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
       <img
@@ -17,6 +18,7 @@ import { CommonModule } from '@angular/common';
       <p class="listing-location">
         {{ housingLocation.city }}, {{ housingLocation.state }}
       </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['housing-location.component.css'],
